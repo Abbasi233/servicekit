67af6240732e65b12b150fdd68bd95806ad5eedf
Furkan Abbasioğlu <furkan_abbasi233@outlook.com>
2023-06-27 14:15:48 +0300

    bug fixed

--- lib/servicekit.dart
+++ lib/servicekit.dart
@@ -61,7 +61,7 @@ abstract class ServiceKit {
     final firstMatch = phoneRegEx.firstMatch(phoneNumber);
     final phoneInCorrectForm = (firstMatch?.group(1)?.isNotEmpty ?? false ? "+${firstMatch!.group(1)!}" : "+90") + firstMatch!.group(2)!;
 
-    await _inst.setSettings(appVerificationDisabledForTesting: true, forceRecaptchaFlow: false);
+    // await _inst.setSettings(appVerificationDisabledForTesting: true, forceRecaptchaFlow: false);
     await _inst.verifyPhoneNumber(
       timeout: timeout,
       forceResendingToken: 0,
@@ -88,6 +88,7 @@ abstract class ServiceKit {
 
       var doc = await userDoc.get();
       if (!doc.exists) await userDoc.set(userDocumentMap);
+      await currentUser?.updatePhoneNumber(credential);
     } on FirebaseAuthException catch (e) {
       onError(Errors.fromFirebase(e));
     }
